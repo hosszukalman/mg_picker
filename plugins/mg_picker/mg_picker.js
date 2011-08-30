6d766be8f982b5179a0f59ee0abe3fc860b6386a
Kalman Hosszu <hosszu.kalman@gmail.com>
2011-08-30 19:47:37 +0200

    Open the dialog in js.

--- plugins/mg_picker/mg_picker.js
+++ plugins/mg_picker/mg_picker.js
@@ -14,7 +14,7 @@ Drupal.wysiwyg.plugins['mg_picker'] = {
    * Execute the button.
    */
   invoke: function(data, settings, instanceId) {
-    
+    Drupal.wysiwyg.instances[instanceId].openDialog(settings.dialog, data);
   }
 }
   
