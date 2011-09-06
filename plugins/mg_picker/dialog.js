a53aa25c1bf330c74e0f18d3457041fd8ba58dde
Kalman Hosszu <hosszu.kalman@gmail.com>
2011-09-06 10:43:15 +0200

    Add cancel click to dialog js file.

--- plugins/mg_picker/dialog.js
+++ plugins/mg_picker/dialog.js
@@ -8,5 +8,18 @@
 
 (function ($) {
 
+  /**
+   * Cancel button click
+   */
+  Drupal.behaviors.mgPickerCloseDialog = {
+    attach: function (context, settings) {
+      $('#mg-picker-cancel', context).click(function () {
+        var dialog = Drupal.settings.wysiwyg;
+        var target = window.opener || window.parent;
+        target.Drupal.wysiwyg.instances[dialog.instance].closeDialog(window);
+        return false;
+      });
+    }
+  };
 
 })(jQuery);
