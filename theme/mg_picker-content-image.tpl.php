8647a457e13317a3b6f0e7c94576217bf99b5369
Kalman Hosszu <hosszu.kalman@gmail.com>
2011-09-17 16:04:15 +0200

    Add image content template file.

---
+++ theme/mg_picker-content-image.tpl.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ *
+ * Available variables:
+ * - $image_link
+ *   The link to the gallery image.
+ * - $image_title
+ *   The title of the image entity.
+ * - $photographer
+ *   The photographer term link.
+ *
+ *
+ * @author Kálmán Hosszu - hosszu.kalman@gmail.com - http://premiumcmsthemes.com - http://www.kalman-hosszu.com
+ */
+?>
+<?php
+echo $image_link;
+echo $image_title;
+echo $photographer;
+?>
